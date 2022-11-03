08fd86e0df728d6a8097eb6b3ab0df7674180bf7
FolkererD <84351112+FolkererD@users.noreply.github.com>
2022-11-03 18:03:16 +0100

    Update for optional attributes for update account

--- Application/UseCases/Account/Dtos/DtoInputUpdateAccount.cs
+++ Application/UseCases/Account/Dtos/DtoInputUpdateAccount.cs
@@ -8,14 +8,14 @@ public class DtoInputUpdateAccount
     // public Account account { get; set; }
     public int Id { get; set; }
     public string Email { get; set; }
-    // public string Password { get; set; }
-    public string LastName { get; set; }
-    public string FirstName { get; set; }
-    // public string Street {get; set;}
-    // public string Number {get; set;}
-    // public int PostCode {get; set;}
-    // public string City {get; set;}
-    // public bool IsChief {get; set;}
-    // public string PictureURL {get; set;}
-    public string Function { get; set; }
+    public string? Password { get; set; }
+    public string? LastName { get; set; }
+    public string? FirstName { get; set; }
+    public string? Street {get; set;}
+    public string? Number {get; set;}
+    public int? PostCode {get; set;}
+    public string? City {get; set;}
+    public bool? IsChief {get; set;}
+    public string? PictureURL {get; set;}
+    public string? Function { get; set; }
 }
